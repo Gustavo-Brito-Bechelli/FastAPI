ec1399417bfefaf3ae8f84c8db13f067bbd2d3c2
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-13 11:14:37 -0300

    feat(books2.py): criando validação para adicionar um novo livro

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()     # uvicorn books2:app --reload
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = [
     Book(1, 'Computer Science', 'Gustavo Brito', 'A very good book', 5),
     Book(2, 'Learning Django', 'codingwithroby', 'A excelent book', 5),
@@ -33,6 +42,8 @@ async def read_all_books():
     return BOOKS
 
 
-@app.post("/creat-book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())  # Book(**book_request.dict()) == converter a requesição para Book objeto
+    # Book(**book_request.model_dump()) == caso o ".dict()" esteja dando erro
+    BOOKS.append(new_book)
