db6722ffbffd4be5e59e32d429a73fd2f879e811
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-13 10:48:19 -0300

    feat(books2.py): criando metodo para adicionar um novo livro

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()     # uvicorn books2:app --reload
 
@@ -31,3 +31,8 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/creat-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
