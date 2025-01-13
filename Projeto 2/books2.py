4e7a37144f213fb795aeba7f39f132c6c690881b
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-13 11:51:28 -0300

    feat(books2.py): colocando um modelo de exemplo para adicionar um novo livro

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -22,11 +22,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None    # tornando o id opcional
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)    # tornando o id opcional
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "Gustavo Brito",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 
 BOOKS = [
