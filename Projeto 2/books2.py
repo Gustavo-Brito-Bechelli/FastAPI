2ea6341bfe24dd2d1d5a162b65c96e51cddee761
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-13 12:19:13 -0300

    feat(books2.py): adicionando novos metodos GET, POST, PUT e DELETE

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -55,6 +55,22 @@ async def read_all_books():
     return BOOKS
 
 
+@app.get("/books/{book-id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+
+@app.get("books/")
+async def create_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())  # Book(**book_request.dict()) == converter a requesição para Book objeto
@@ -73,5 +89,19 @@ def find_book_id(book: Book):
     #     book.id = BOOKS[-1].id + 1
     # else:
     #     book.id = 1
-
     return book
+
+
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
