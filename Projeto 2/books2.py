1ab0bf64d7741fe4d8805e12ff425dbd66e13118
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-14 10:46:09 -0300

    feat(books2.py): validações nos parametros GET

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from pydantic import BaseModel, Field
 
 app = FastAPI()     # uvicorn books2:app --reload
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_date: int
 
-    def __init__(self, id, title, author, description, rating):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
 class BookRequest(BaseModel):
@@ -27,6 +29,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     model_config = {
         "json_schema_extra": {
@@ -34,19 +37,20 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "Gustavo Brito",
                 "description": "A new description of a book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2029
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Computer Science', 'Gustavo Brito', 'A very good book', 5),
-    Book(2, 'Learning Django', 'codingwithroby', 'A excelent book', 5),
-    Book(3, 'HP1', 'Author 1', 'Book Description', 1),
-    Book(4, 'HP2', 'Author 2', 'Book Description', 2),
-    Book(5, 'HP3', 'Author 3', 'Book Description', 4),
-    Book(6, 'FastAPI', 'Author 4', 'Book Description', 8)
+    Book(1, 'Computer Science', 'Gustavo Brito', 'A very good book', 5, 2030),
+    Book(2, 'Learning Django', 'codingwithroby', 'A excelent book', 5, 2025),
+    Book(3, 'HP1', 'Author 1', 'Book Description', 1, 2028),
+    Book(4, 'HP2', 'Author 2', 'Book Description', 2, 2040),
+    Book(5, 'HP3', 'Author 3', 'Book Description', 4, 2020),
+    Book(6, 'FastAPI', 'Author 4', 'Book Description', 8, 2015)
 ]
 
 
@@ -56,7 +60,7 @@ async def read_all_books():
 
 
 @app.get("/books/{book-id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -67,6 +71,15 @@ async def create_book_by_rating(book_rating: int):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/publish/")
+async def read_book_by_publish_date(published_date: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
             books_to_return.append(book)
     return books_to_return
 
@@ -100,7 +113,7 @@ async def update_book(book: BookRequest):
 
 
 @app.delete("/books/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
