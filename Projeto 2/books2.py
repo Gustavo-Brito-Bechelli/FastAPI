0b7498c0f70ccdecb4191eae757b697534b5be23
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-01-14 10:51:53 -0300

    feat(books2.py): validações nos parametros de busca

--- Projeto 2/books2.py
+++ Projeto 2/books2.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
 
 app = FastAPI()     # uvicorn books2:app --reload
@@ -48,7 +48,7 @@ BOOKS = [
     Book(1, 'Computer Science', 'Gustavo Brito', 'A very good book', 5, 2030),
     Book(2, 'Learning Django', 'codingwithroby', 'A excelent book', 5, 2025),
     Book(3, 'HP1', 'Author 1', 'Book Description', 1, 2028),
-    Book(4, 'HP2', 'Author 2', 'Book Description', 2, 2040),
+    Book(4, 'HP2', 'Author 2', 'Book Description', 2, 2030),
     Book(5, 'HP3', 'Author 3', 'Book Description', 4, 2020),
     Book(6, 'FastAPI', 'Author 4', 'Book Description', 8, 2015)
 ]
@@ -67,7 +67,7 @@ async def read_book(book_id: int = Path(gt=0)):
 
 
 @app.get("books/")
-async def create_book_by_rating(book_rating: int):
+async def create_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -76,7 +76,7 @@ async def create_book_by_rating(book_rating: int):
 
 
 @app.get("/books/publish/")
-async def read_book_by_publish_date(published_date: int):
+async def read_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in BOOKS:
         if book.published_date == published_date:
